e9b916b754db0b341ee67b3f57162b0ea9ab832e
glennc <glennc@microsoft.com>
2016-10-06 10:07:37 -0700

    First pass of Web and API app with basic healthcheck templated

---
+++ Web/src/Web/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseUrls("http://localhost:5000")
+                .UseUrls("http://localhost:8080")
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
